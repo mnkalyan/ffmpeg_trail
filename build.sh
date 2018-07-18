d504ffa6b856fe9e93410d0798cea2cf58eb8f08
Kalyan MN <kalyan460@rediffmail.com>
2018-07-18 13:29:32 -0700

    Update build.sh
    Signed-off-by: Kalyan MN <kalyan460@rediffmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -xe
 CWD=$(echo $PWD)
-rm -fr $CWD/bin
+rm -fr $CWD/output_bin
 ./configure --disable-x86asm
 make clean
 make -j8
-
+top_commit=$(git log --pretty=format:"%H" -- . ":(exclude,icase)output_bin")
 echo "Code compiled successfully, copying images to bin folder"
-mkdir -p $CWD/bin
-cp -fr ffmpeg* $CWD/bin
-touch "$CWD/bin/build_done.txt"
-
+mkdir -p $CWD/output_bin
+echo "$top_commit"=$CWD/output_bin/commit_tracker.txt
+cp -fr ffmpeg* $CWD/output_bin
+touch "$CWD/output_bin/build_done.txt"
