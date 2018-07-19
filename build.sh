08a03c4f8cf85382e93780edd5c53f3006bb604a
Kalyan MN <kalyan460@rediffmail.com>
2018-07-19 18:27:46 +0530

    Update build.sh
    Signed-off-by: Kalyan MN <kalyan460@rediffmail.com>

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -xe
 CWD=$(echo $PWD)
-rm -fr $CWD/output_bin
+rm -fr $CWD/output_bin/bin
 ./configure --disable-x86asm
 make clean
 make -j8
 top_commit=$(git log --pretty=format:"%H" -- . ":(exclude,icase)output_bin" | head -1)
 echo "Code compiled successfully, copying images to bin folder"
-mkdir -p $CWD/output_bin
-echo "$top_commit" > $CWD/commit_tracker.txt
+mkdir -p $CWD/output_bin/bin
+echo "$top_commit" > $CWD/output_bin/commit_tracker.txt
 cp -fr ffmpeg* $CWD/output_bin
 
 echo "Update output_bin in github for testing"
