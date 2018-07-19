18fed9728248df135330a54c0b0bf10078a847bf
mnkalyan <41343963+mnkalyan@users.noreply.github.com>
2018-07-20 00:51:15 +0530

    Update swscale.c

--- libswscale/swscale.c
+++ libswscale/swscale.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2001-2011 Michael Niedermayer <michaelni@gmx.at>
  *
- * This file is part of FFmpeg.
+ * This file is part of FFmpeg
  *
  * FFmpeg is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
